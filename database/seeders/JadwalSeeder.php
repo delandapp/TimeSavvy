251390294f9e448f5ce6e63dfa757de3d0926bb1
delandapp <alihandronikolas@gmail.com>
2024-05-24 23:43:38 +0700

    add seeder jadwal

--- database/seeders/JadwalSeeder.php
+++ database/seeders/JadwalSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\jadwal;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class JadwalSeeder extends Seeder
 {
@@ -12,6 +14,23 @@ class JadwalSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Schema::disableForeignKeyConstraints();
+        jadwal::truncate();
+        Schema::enableForeignKeyConstraints();
+        $data_jadwal = [
+            
+                ['nama' => 'Senin', 'waktu' => null],
+                ['nama' => 'Selasa', 'waktu' => null],
+                ['nama' => 'Rabu', 'waktu' => null],
+                ['nama' => 'Kamis', 'waktu' => null],
+                ['nama' => 'Jumat', 'waktu' => null],
+        
+        ];
+        foreach($data_jadwal as $data) :
+            jadwal::insert([
+                'hari' => $data['nama'],
+                'waktu' => $data['waktu'],
+            ]);
+        endforeach;
     }
 }
