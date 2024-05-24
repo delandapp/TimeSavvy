4d6b75e0dd0f725d72ebf6ac818e8ffe4d9f87cf
delandapp <alihandronikolas@gmail.com>
2024-05-24 23:45:49 +0700

    add seeder users

--- database/seeders/DetailUserSeeder.php
+++ database/seeders/DetailUserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\detail_user;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Schema;
 
 class DetailUserSeeder extends Seeder
 {
@@ -12,6 +15,32 @@ class DetailUserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Schema::disableForeignKeyConstraints();
+        User::truncate();
+        Schema::enableForeignKeyConstraints();
+        $data_siswa = [
+    
+                ['name' => 'Muhammad Deland Arjuna Putra', 'email' => 'muhammad.deland36@smk.belajar.id', 'password' => 'admin', 'id_roles' => 1,'id' => 1],
+                ['name' => 'Yogha Irgi Kurniawan', 'email' => 'yogha.irgi36@smk.belajar.id', 'password' => 'admin', 'id_roles' => 1, 'id' => 2],
+                ['name' => 'Geusan Edurais', 'email' => 'geusan.edurais36@smk.belajar.id', 'password' => 'admin', 'id_roles' => 1, 'id' => 3],
+                ['name' => 'Admin', 'email' => 'admin@smk.belajar.id', 'password' => 'admin', 'id_roles' => 3, 'id' => 4],
+            
+        ];
+        foreach($data_siswa as $data) :
+            User::create([
+                'name' => $data['name'],
+                'email' => $data['email'],
+                'status' => 'verify',
+                'password' => bcrypt($data['password']),
+                'id_roles' => $data['id_roles'],
+            ]);
+            detail_user::create([
+                'id_user' => $data['id'],
+                'id_alamat' => $data['id'],
+                'id_level' => 1,
+                'id_jurusan' => random_int(1, 3),
+                'id_kelas' => random_int(1, 3),
+            ]);
+        endforeach;
     }
 }
